9fbd887e3d1b09a39a27224d737ceb6b609ff4d0
andika-eka <putuandika387@gmail.com>
2021-11-11 23:27:11 +0800

    model and migration done.

---
+++ database/migrations/2021_11_11_134757_create_wisatas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWisatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wisatas', function (Blueprint $table) {
+            $table->id("Id_Wisata");
+            $table->string("Nama_Wisata");
+            $table->text("Deskripsi")->nullable();
+            $table->integer("User_id");
+            $table->string("Foto")->nullable();
+            $table->text("Lokasi")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wisatas');
+    }
+}
---
+++ database/migrations/2021_11_11_135401_create_ratings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id("Id_Rating");
+            $table->integer("Wisata_id");
+            $table->decimal("Hasil_Rating", $precision = 3, $scale = 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
