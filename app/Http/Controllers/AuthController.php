d763d8874ef7335995879318d75fcda82f0183f8
4gunkbgs <51852975+4gunkbgs@users.noreply.github.com>
2021-11-15 13:10:49 +0800

    UI Login improve

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,14 +7,12 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function login(){
-
+        
         return view('auth.login');
-
     }
 
     public function register(){
 
         return view('auth.register');
-
     }
 }
